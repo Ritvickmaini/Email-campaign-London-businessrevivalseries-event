7517bbe9eff4b5a237b55360e061b76e97d74b2c
Ritvick Maini <101634547+Ritvickmaini@users.noreply.github.com>
2026-01-15 15:43:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,6 @@ def send_email(recipient, first_name, subject, html_body):
         <strong>Mark Randell</strong><br>
         Marketing Executive | B2B Growth Expo<br>
         <a href="mailto:mark@londonbusinessshow.com" style="color:#000;text-decoration:none;">mark@londonbusinessshow.com</a><br>
-        (+44) 2034517166
     </div>"""
 
     unsubscribe_section = f"""
